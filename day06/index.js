5c18af3856f872df30b69cc72c281889f1587e8d
cyntl3r <damian@cyntler.com>
2020-12-06 23:59:16 +0100

    Day 6 solutions

---
+++ day06/index.js
@@ -0,0 +1,50 @@
+/**
+ * Advent of Code 2020
+ * @author cyntl3r
+ * @description https://adventofcode.com/2020/day/6
+ */
+import { readInput } from '../utils.js';
+
+const getCountOfAnyoneYesAnswered = (data) => {
+  const answeredLetters = {};
+  data
+    .split('\n')
+    .filter((line) => line)
+    .forEach((line) => {
+      line.split('').forEach((letter) => {
+        answeredLetters[letter] = null;
+      });
+    });
+  return Object.keys(answeredLetters).length;
+};
+
+const getCountOfEveryoneYesAnswered = (data) => {
+  let allLetters = {};
+  let peopleCount = 0;
+  data
+    .split('\n')
+    .filter((line) => line)
+    .forEach((line) => {
+      peopleCount += 1;
+      line.split('').forEach((letter) => {
+        if (!allLetters[letter]) allLetters[letter] = 1;
+        else allLetters[letter] += 1;
+      });
+    });
+  return Object.keys(allLetters).filter(
+    (key) => allLetters[key] === peopleCount
+  ).length;
+};
+
+const findResult = (input) => ({
+  part1: input
+    .map(getCountOfAnyoneYesAnswered)
+    .reduce((prev, next) => prev + next, 0),
+  part2: input
+    .map(getCountOfEveryoneYesAnswered)
+    .reduce((prev, next) => prev + next, 0),
+});
+
+const input = readInput(true).toString();
+const result = findResult(input);
+console.log(result.part1, result.part2);
