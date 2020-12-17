cf5a691e845ab9e43beba3a8dedf22c8dfa3ac79
cyntl3r <damian@cyntler.com>
2020-12-17 13:09:41 +0100

    Add day 12 solution

---
+++ day12/index.js
@@ -0,0 +1,116 @@
+/**
+ * Advent of Code 2020
+ * @author cyntl3r
+ * @description https://adventofcode.com/2020/day/12
+ */
+import { getInputPath, readInput } from '../utils.js';
+
+const getManhattanDistance = (input, isWaypoint = false) => {
+  const directions = {
+    E: 0,
+    N: 1,
+    W: 2,
+    S: 3,
+  };
+  const shipPosition = [0, 0];
+  let direction = directions.E;
+  let waypointPosition = [1, 10];
+  for (const instruction of input) {
+    const [action, ...value] = instruction.split('');
+    const intValue = parseInt(value.join(''), 10);
+    switch (action) {
+      case 'N': {
+        if (isWaypoint) {
+          waypointPosition[0] += intValue;
+          break;
+        }
+        shipPosition[0] += intValue;
+        break;
+      }
+      case 'S': {
+        if (isWaypoint) {
+          waypointPosition[0] -= intValue;
+          break;
+        }
+        shipPosition[0] -= intValue;
+        break;
+      }
+      case 'W': {
+        if (isWaypoint) {
+          waypointPosition[1] -= intValue;
+          break;
+        }
+        shipPosition[1] -= intValue;
+        break;
+      }
+      case 'E': {
+        if (isWaypoint) {
+          waypointPosition[1] += intValue;
+          break;
+        }
+        shipPosition[1] += intValue;
+        break;
+      }
+      case 'L': {
+        if (isWaypoint) {
+          for (let i = 0; i < intValue / 90; i += 1) {
+            waypointPosition = waypointPosition.reverse();
+            waypointPosition[1] = -1 * waypointPosition[1];
+          }
+          break;
+        }
+        direction = (direction + intValue / 90 + 4) % 4;
+        break;
+      }
+      case 'R': {
+        if (isWaypoint) {
+          for (let i = 0; i < intValue / 90; i += 1) {
+            waypointPosition = waypointPosition.reverse();
+            waypointPosition[0] = -1 * waypointPosition[0];
+          }
+          break;
+        }
+        direction = (direction - intValue / 90 + 4) % 4;
+        break;
+      }
+      case 'F': {
+        if (isWaypoint) {
+          shipPosition[0] += waypointPosition[0] * intValue;
+          shipPosition[1] += waypointPosition[1] * intValue;
+          break;
+        }
+        switch (direction) {
+          case directions.N: {
+            shipPosition[0] += intValue;
+            break;
+          }
+          case directions.S: {
+            shipPosition[0] -= intValue;
+            break;
+          }
+          case directions.W: {
+            shipPosition[1] -= intValue;
+            break;
+          }
+          case directions.E: {
+            shipPosition[1] += intValue;
+            break;
+          }
+        }
+        break;
+      }
+    }
+  }
+  return Math.abs(shipPosition[0]) + Math.abs(shipPosition[1]);
+};
+
+export const findResult = (input) => ({
+  part1: getManhattanDistance(input),
+  part2: getManhattanDistance(input, true),
+});
+
+const input = readInput(
+  getInputPath(import.meta.url, './input.txt')
+).toString();
+const result = findResult(input);
+console.log(result.part1, result.part2);
---
+++ day12/index.test.js
@@ -0,0 +1,11 @@
+import { getInputPath, readInput } from '../utils';
+import { findResult } from '.';
+
+test('day 12 testing', () => {
+  const input = readInput(
+    getInputPath(import.meta.url, './input.txt')
+  ).toString();
+  const { part1, part2 } = findResult(input);
+  expect(part1).toBe(420);
+  expect(part2).toBe(42073);
+});
