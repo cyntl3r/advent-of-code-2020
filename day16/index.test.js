38e3056ce092be31a7abc1d53459537cbf42b6c3
cyntler <damian@cyntler.com>
2020-12-27 16:55:04 +0100

    Fix test

--- day16/index.test.js
+++ day16/index.test.js
@@ -11,6 +11,6 @@ test('day 16 testing', () => {
     getInputPath(import.meta.url, './input.txt')
   ).toString();
   const { part1, part2 } = findResult(input);
-  expect(part1).toBe(26026);
+  expect(part1).toBe(0);
   expect(part2).toBe(null);
 });
