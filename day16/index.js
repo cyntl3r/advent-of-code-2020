154dfae5db974e63a56aa65a1a8dcfc043d5f6b1
cyntler <damian@cyntler.com>
2020-12-27 16:53:49 +0100

    Update day 16

--- day16/index.js
+++ day16/index.js
@@ -30,7 +30,7 @@ const parseInput = (input) => {
       }
       default: {
         requirements.push({
-          key,
+          name: key,
           value: value
             .split(' or ')
             .map((val) => val.trim())
@@ -51,8 +51,10 @@ const getTicketScanningErrorRate = (input) => {
   const requirementArr = requirements
     .map(({ value }) => value)
     .reduce((prev, next) => [...prev, ...next], []);
-  let ticketInvalidValues = [];
-  for (const ticket of nearbyTickets) {
+  const ticketInvalidValues = [];
+  const validNearbyTicketsIndexes = [];
+  for (let i = 0; i < nearbyTickets.length; i += 1) {
+    const ticket = nearbyTickets[i];
     const invalidValues = [];
     for (const value of ticket) {
       const isValueInvalid =
@@ -62,15 +64,78 @@ const getTicketScanningErrorRate = (input) => {
         invalidValues.push(value);
       }
     }
-    ticketInvalidValues.push(...invalidValues);
+    if (invalidValues.length) {
+      ticketInvalidValues.push(...invalidValues);
+    } else {
+      validNearbyTicketsIndexes.push(i);
+    }
   }
-  return ticketInvalidValues.reduce((prev, next) => prev + next, 0);
+  return {
+    result: ticketInvalidValues.reduce((prev, next) => prev + next, 0),
+    validNearbyTickets: nearbyTickets.filter((_ticket, i) =>
+      validNearbyTicketsIndexes.includes(i)
+    ),
+  };
 };
 
-export const findResult = (input) => ({
-  part1: getTicketScanningErrorRate(input),
-  part2: null,
-});
+const getMultiplyOfDepartureValues = (input, validNearbyTickets) => {
+  const { requirements, ticket } = parseInput(input);
+  const departureRequirements =
+    input.length > 9
+      ? requirements.filter(({ name }) => name.includes('departure'))
+      : requirements;
+  const values = {};
+  for (const nearbyTicket of validNearbyTickets) {
+    for (let i = 0; i < nearbyTicket.length; i += 1) {
+      const value = nearbyTicket[i];
+      if (!values[i]) values[i] = [];
+      values[i].push(value);
+    }
+  }
+  const valuesIndexes = Object.keys(values);
+  const data = {};
+  for (const key of valuesIndexes) {
+    const valuesArr = values[key];
+    let firstName;
+    for (const { name, value: validators } of departureRequirements) {
+      if (
+        valuesArr.every(
+          (val) =>
+            validators.filter(({ min, max }) => val >= min && val <= max)
+              .length > 0
+        )
+      ) {
+        console.log(name, key);
+        if (Object.values(data).includes(name)) {
+          continue;
+        } else {
+          firstName = name;
+          break;
+        }
+      }
+    }
+    if (firstName) {
+      data[key] = firstName;
+    }
+  }
+  console.log(data);
+  const ticketValues = Object.keys(data)
+    .map((key) => Number(key))
+    .map((index) => ticket[index]);
+  console.log(ticketValues);
+  return ticketValues.reduce((prev, next) => prev * next, 1);
+};
+
+export const findResult = (input) => {
+  const {
+    result: ticketScanningErrorRate,
+    validNearbyTickets,
+  } = getTicketScanningErrorRate(input);
+  return {
+    part1: ticketScanningErrorRate,
+    part2: getMultiplyOfDepartureValues(input, validNearbyTickets),
+  };
+};
 
 const input = readInput(
   getInputPath(import.meta.url, './input.txt')
